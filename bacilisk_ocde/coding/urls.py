ad23b6c3b812fe72abc0b0998ee6eb9564544ccf
Renati Saathwick Reddy <saathwickreddyr@cse.iitb.ac.in>
2020-12-06 23:37:11 +0530

    changes to urls.py in coding folder

--- bacilisk_ocde/coding/urls.py
+++ bacilisk_ocde/coding/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    
+    path('', views.index, name='index'),
+    path('savecode', views.savecode, name="savecode"),
+    path('runcode/', views.runcode, name="runcode"),
 ]
